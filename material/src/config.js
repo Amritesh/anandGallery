8150bf2b4a973deb034fbee8d8060208c5366c0f
rajatmann-nocd <rajat.mann@techjini.net>
2019-06-12 22:28:14 +0530

    config

---
+++ material/src/config.js
@@ -0,0 +1,13 @@
+export const env = 'staging'
+
+export const firebase = {
+  apiKey: 'AIzaSyCu0U3Ov6RQJIrpnk6syc0asXHYmIq0Qzc',
+  authDomain: 'anandmuseum.firebaseapp.com',
+  databaseURL: 'https://anandmuseum.firebaseio.com',
+  projectId: 'anandmuseum',
+  storageBucket: 'anandmuseum.appspot.com'
+}
+
+export const reduxFirebase = {}
+
+export default { env, firebase, reduxFirebase }
